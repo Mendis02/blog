739cc9f98ec02683674d975ef90d660747637ccb
thanura mendis <thanuramendis02@gmail.com>
2023-01-17 12:51:20 +0530

    Main page working

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,25 +1,30 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
+const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/'
 
 const PostsMain = () => {
-    const [post,setPost] = useState([])
-    
-    useEffect( () => {    
+    const [post, setPost] = useState([])
+   
+    useEffect(() => {
         getDataFromApi()
     }, [])
 
     const getDataFromApi = () => {
         fetch(API_ENDPOINT)
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            // .then((data) => console.log(data))
             .then((data) => setPost(data))
-            // .catch(err => console.error(err));
+            .catch(err => console.error(err));
     }
 
-    return(
+    return (
         <div>
-          {post.map(p => < post id={p.id} title={p.title} intro={p.body}/>)}
+            {post.map(p =>
+                <div key={p.id}>
+                    <h1>{p.id}</h1>
+                    <h1>{p.title}</h1>
+                </div>
+            )}
         </div>
     )
 }
