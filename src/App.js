4242906be23235ba9243da46f3388d830892004c
thanura mendis <thanuramendis02@gmail.com>
2023-01-21 21:51:08 +0530

    switches between 2 pages onClick & firebase hosted

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-<<<<<<< HEAD
-=======
-    Link
->>>>>>> master
 } from "react-router-dom";
 
 import Navbar from "./Components/Navbar"
@@ -19,27 +15,16 @@ export default function App() {
             <Navbar />
             <Router>
                 <Switch>
-<<<<<<< HEAD
                     {/* what does the switch do? Cause on click the pages switches without errors even without the 'Switch' */}
-=======
-                    <Route path="/:id" children={<PostDetails />}>
-                    </Route>
->>>>>>> master
 
                     <Route exact path="/" >
                         <PostsMain />
                     </Route>
-<<<<<<< HEAD
                     <Route path="/:id" >
                         <PostDetails />
                     </Route>
                 </Switch>
             </Router>
         </div >
-=======
-                </Switch>
-            </Router>
-        </div>
->>>>>>> master
     )
 }
--- src/Screens/Posts.js
+++ src/Screens/Posts.js
@@ -1,24 +1,10 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD:src/Screens/Posts.js
 import { Link } from "react-router-dom";
-=======
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-
->>>>>>> master:src/Components/Posts.js
 
 const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/'
 
 const PostsMain = () => {
-<<<<<<< HEAD:src/Screens/Posts.js
     const [posts, setPosts] = useState([])
-=======
-    const [post, setPost] = useState([])
->>>>>>> master:src/Components/Posts.js
 
     useEffect(() => {
         getDataFromApi()
@@ -32,7 +18,6 @@ const PostsMain = () => {
     }
 
     return (
-<<<<<<< HEAD:src/Screens/Posts.js
         <div>
             {posts?.map(p =>
                 <div key={p.id}>
@@ -41,20 +26,6 @@ const PostsMain = () => {
                 </div>
             )}
         </div>
-=======
-        <Router>
-            <div>
-                <Link to="/"> Home </Link>
-                {post.map(p =>
-                    <div key={p.id} className="card">
-                        <h2>{p.id}</h2>
-                        <Link to={"/" + p.id} className="card-title">{p.title}</Link>
-                    </div>
-                )}
-
-            </div>
-        </Router>
->>>>>>> master:src/Components/Posts.js
     )
 }
 
